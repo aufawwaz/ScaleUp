76eef479e52eb8ead8bd81775c0afc28e9f91a75
Belajar Git <arilfadlahuda1234@student.uns.ac.id>
2025-06-02 15:15:06 +0700

    add color

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard - ScaleUp</title>
+  @vite('resources/css/app.css')
+</head>
+<body class="">
+  <nav class="flex flex-col">
+    <a href="/">Dashboard</a>
+    <a href="/">Kelola Produk</a>
+    <a href="/">Transaksi</a>
+    <a href="/">Saldo</a>
+    <a href="/">Pelanggan</a>
+    <a href="/">Berita</a>
+    <a href="/">Logout</a>
+  </nav>
+</body>
+</html>
