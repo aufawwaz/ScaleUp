ac99694c2dac8e4ad3418b1688313ccb4f6cfdaf
Ariel Josua <arieljo145@gmail.com>
2025-06-13 21:11:26 +0700

    fix merging web.php

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.process');
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
 Route::post('/register', [AuthController::class, 'register'])->name('register.process');
 Route::get('/auth-google-redirect', [AuthController::class, 'google_redirect'])->name('google.redirect');
 Route::get('/auth-google-callback', [AuthController::class, 'google_callback'])->name('google.callback');
@@ -31,16 +32,16 @@ Route::get('/auth-google-callback', [AuthController::class, 'google_callback'])-
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// Profile
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::get('/profile/get', [ProfileController::class, 'getUserData']);
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile/get', [ProfileController::class, 'getUserData']);
 
-// Saldo
-Route::get('saldo/fetch/{id}', [SaldoController::class, 'getTransactionHistory'])->name('saldo.fetch');
-Route::resource('saldo', SaldoController::class);
-Route::get('/saldo/getById/{id}', [SaldoController::class, 'getById']);
+    // Saldo
+    Route::get('saldo/fetch/{id}', [SaldoController::class, 'getTransactionHistory'])->name('saldo.fetch');
+    Route::resource('saldo', SaldoController::class);
+    Route::get('/saldo/getById/{id}', [SaldoController::class, 'getById']);
 
     // Product
     Route::resource('product', ProductController::class)->parameters([
@@ -48,35 +49,35 @@ Route::get('/saldo/getById/{id}', [SaldoController::class, 'getById']);
         'backlink' => ''
     ]);
 
-// Contact
-Route::resource('contact', ContactController::class);
-Route::get('/contact/getById/{id}', [ContactController::class, 'getById']);
+    // Contact
+    Route::resource('contact', ContactController::class);
+    Route::get('/contact/getById/{id}', [ContactController::class, 'getById']);
 
     // News
     Route::get('/news/fetch', [NewsController::class, 'fetchNewsHTML'])->name('news.fetch');
     Route::get('/news', [NewsController::class, 'index'])->name('news');
     Route::get('/news/{id}', [NewsController::class, 'show'])->name('news.show');
 
-// Transaksi
-Route::get('/transaction', function () { return view('transaction'); })->name('transaction');
-Route::get('/transaction/get-product', [TransactionController::class, 'getProductById'])->name('getProduct');
-Route::post('/transaction/store', [TransactionController::class, 'store'])->name('transaction.store');
-Route::get('/sale', [TransactionController::class, 'indexSale'])->name('sale');
-Route::get('/purchase', [TransactionController::class, 'indexPurchase']) ->name('purchase');
-Route::get('/bill', [TransactionController::class, 'indexBill'])->name('bill');
-Route::get('/autocomplete/contact', [ContactController::class, 'autocomplete'])->name('contact.autocomplete');
-Route::get('/autocomplete/saldo', [SaldoController::class, 'autocomplete'])->name('saldo.autocomplete');
+    // Transaksi
+    Route::get('/transaction', function () { return view('transaction'); })->name('transaction');
+    Route::get('/transaction/get-product', [TransactionController::class, 'getProductById'])->name('getProduct');
+    Route::post('/transaction/store', [TransactionController::class, 'store'])->name('transaction.store');
+    Route::get('/sale', [TransactionController::class, 'indexSale'])->name('sale');
+    Route::get('/purchase', [TransactionController::class, 'indexPurchase']) ->name('purchase');
+    Route::get('/bill', [TransactionController::class, 'indexBill'])->name('bill');
+    Route::get('/autocomplete/contact', [ContactController::class, 'autocomplete'])->name('contact.autocomplete');
+    Route::get('/autocomplete/saldo', [SaldoController::class, 'autocomplete'])->name('saldo.autocomplete');
 
-// Authentication
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+    // Authentication
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+    Route::get('/register', function () {
+        return view('register');
+    })->name('register');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login.process');
-Route::post('/register', [AuthController::class, 'register'])->name('register.process');
-Route::get('/auth-google-redirect', [AuthController::class, 'google_redirect'])->name('google.redirect');
-Route::get('/auth-google-callback', [AuthController::class, 'google_callback'])->name('google.callback');
-}
+    Route::post('/login', [AuthController::class, 'login'])->name('login.process');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.process');
+    Route::get('/auth-google-redirect', [AuthController::class, 'google_redirect'])->name('google.redirect');
+    Route::get('/auth-google-callback', [AuthController::class, 'google_callback'])->name('google.callback');
+});
