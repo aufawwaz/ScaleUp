21af8bee0a18b9904fe4483e2389bbb7a2af36c5
Belajar Git <arilfadlahuda1234@student.uns.ac.id>
2025-06-16 17:36:28 +0700

    .

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -253,4 +253,10 @@ class TransactionController extends Controller
             return back()->with('error', 'Gagal update status');
         }
     }
+    public function getAllData(){
+        return response()->json([
+            'success' => true,
+            'data' => Transaction::all()
+        ]);
+    }
 }
