4640b75e852bac2703a0b0b318cd66388b12effd
Belajar Git <arilfadlahuda1234@student.uns.ac.id>
2025-06-16 12:45:16 +0700

    redesign Login dan Register Page

--- resources/views/components/input_form.blade.php
+++ resources/views/components/input_form.blade.php
@@ -1,4 +1,4 @@
-<div class="rounded-[12px] border-1 border-gray p-2 w-full flex gap-2 items-center">
+<div class="text-xs rounded-[12px] border-1 border-gray h-[44px] px-3 w-full flex gap-2 items-center hover:border-primary focus-within:border-primary transition-all">
     @php
         $svg = file_get_contents(public_path($icon));
         $svg = str_replace('fill="#020507"', 'fill="currentColor"', $svg);
@@ -9,7 +9,7 @@
         type="{{ $type }}" 
         name="{{ $name }}"
         placeholder="{{ $placeholder }}" 
-        class="w-[100%] text-{{ $textSize }} border-none focus:border-none focus:ring-0 outline-none leading-tight"
+        class="w-full text-xs text-{{ $textSize }} truncate border-none focus:border-none focus:ring-0 focus:truncate outline-none leading-tight"
         autocomplete="{{ $autoComplete }}"
     >
 </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,80 +6,98 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <title>Register ScaleUp</title>
+
+    <!-- AOS Library -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </head>
-<body style="background: linear-gradient(#007AFF, #0E315D); overflow-y: auto;" class="min-h-screen md:h-full">
-    <div class="flex flex-col md:flex-row md:items-center md:justify-between px-[5vw] py-[3vh] md:h-[100vh] gap-[6vh] md:gap-[3vw]">
+<body style="overflow-y: auto;" class="min-h-dvh bg-white font-poppins">
+    <!-- AOS Initialization -->
+    <script>
+        AOS.init({
+            duration: 500, 
+            once: false, 
+        });
+    </script>
+
+    <div class="flex w-full min-h-dvh justify-center items-center">
         
-        {{-- onboard --}}
-        <div class="fade-move-up relative w-full h-[28rem] md:w-[480px] overflow-hidden text-white md:ml-auto mb-4 md:mb-0 md:mr-0 flex flex-col justify-center gap-2" style="transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;">
-            <div id="auto-scroll" class="flex h-full transition-transform duration-700 ease-in-out">
-                <div class="w-full h-full flex-shrink-0 flex flex-col items-center justify-center px-4">
-                    <img src="/asset/onboard_1.svg" alt="" class="w-[275px] mb-[1rem]">
-                    <h3 class="text-2xl font-bold">Kelola Bisnis dalam Satu Tempat dengan Mudah</h3>
-                    <p class="text-sm">Atur produk, transaksi, saldo, dan pelanggan dengan mudah dalam satu platform</p>
-                </div>
+        <div data-aos="fade-right" class="inset-shadow-sm shadow-2xl overflow-clip bg-primary-gradient flex rounded-[20px] items-center justify-between w-8/12">
+            {{-- onboard --}}
+            <div class="relative w-full h-[28rem] overflow-hidden text-white md:ml-auto mb-4 md:mb-0 md:mr-0 flex flex-col justify-center gap-2" style="transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;">
+                <div data-aos="fade-up" data-aos-delay="300" id="auto-scroll" class="flex h-full transition-transform duration-700 ease-in-out mb-4">
+                    <div class="w-full px-20 h-full flex-shrink-0 flex flex-col items-center justify-center">
+                        <img src="/asset/onboard_1.svg" alt="" class="h-[260px] mb-[1rem]">
+                        <h3 class="text-xl font-bold text-center mb-3">Kelola Bisnis dalam Satu Tempat dengan Mudah</h3>
+                        <p class="text-xs text-center">Atur produk, transaksi, saldo, dan pelanggan dengan mudah dalam satu platform</p>
+                    </div>
 
-                <div class="w-full h-full flex-shrink-0 flex flex-col items-center justify-center px-4">
-                    <img src="/asset/onboard_2.svg" alt="" class="w-[275px] mb-[1rem]">
-                    <h3 class="text-2xl font-bold">Pengetahuan adalah Kunci Bisnis Berkembang</h3>
-                    <p class="text-sm">Pelajari dan Temukan insight bisnis melalui Knowledge Card dan ambil keputusan dengan percaya diri</p>
-                </div>
+                    <div class="w-full h-full px-20 flex-shrink-0 flex flex-col items-center justify-center ">
+                        <img src="/asset/onboard_2.svg" alt="" class="h-[260px] mb-[1rem]">
+                        <h3 class="text-xl font-bold text-center mb-3">Pengetahuan adalah Kunci Bisnis Berkembang</h3>
+                        <p class="text-xs text-center">Pelajari dan Temukan insight bisnis melalui Knowledge Card dan ambil keputusan dengan percaya diri</p>
+                    </div>
 
-                <div class="w-full h-full flex-shrink-0 flex flex-col items-center justify-center px-4">
-                    <img src="/asset/onboard_3.svg" alt="" class="w-[275px] mb-[1rem]">
-                    <h3 class="text-2xl font-bold">Catat & Kendalikan Keuanganmu Secara Real-Time</h3>
-                    <p class="text-sm">Pantau saldo dan transaksi bisnis secara instan untuk pengelolaan yang lebih efisien</p>
+                    <div class="w-full h-full px-20 flex-shrink-0 flex flex-col items-center justify-center ">
+                        <img src="/asset/onboard_3.svg" alt="" class="h-[260px] mb-[1rem]">
+                        <h3 class="text-xl font-bold text-center mb-3">Catat & Kendalikan Keuanganmu Secara Real-Time</h3>
+                        <p class="text-xs text-center">Pantau saldo dan transaksi bisnis secara instan untuk pengelolaan yang lebih efisien</p>
+                    </div>
+                </div>
+                <div data-aos="zoom-in" data-aos-delay="500" class="w-full h-3 flex gap-1.5 justify-center items-center">
+                    <div class="circle current" onclick="getSlide(0)"></div>
+                    <div class="circle" onclick="getSlide(1)"></div>
+                    <div class="circle" onclick="getSlide(2)"></div>
                 </div>
             </div>
-            <div class="w-full h-3 flex gap-1 justify-center items-center">
-                <div class="circle current" onclick="getSlide(0)"></div>
-                <div class="circle" onclick="getSlide(1)"></div>
-                <div class="circle" onclick="getSlide(2)"></div>
-            </div>
-        </div>
 
-        {{-- form register ygy --}}
-        <div class="fade-move-up container bg-white rounded-[20px] px-[3rem] py-[1rem] md:w-1/2 md:max-w-[480px] md:mr-auto md:ml-0" style="transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;">
-            <div class="flex items-center justify-start">
-                <img src="/asset/scaleUp_logo.svg" alt="" class="w-10">
-                <p class="text-primary-900 text-2xl transform -translate-x-1.25">cale<b>Up</b></p>
-            </div>
-
-            <h2 class="text-dark text-3xl text-center font-bold py-[1rem]">Sign Up</h2>
-            <button class="rounded-[12px] border-1 border-gray p-2 w-full flex flex-row justify-center items-center gap-1 cursor-pointer">
-                <img src="/asset/ic_google.svg" alt="" style="width: 12px;">
-                <p class="text-dark text-sm font-bold mb-0.5">Sign up with Google</p>
-            </button>
-
-            <div class="flex items-center justify-center gap-x-[8px] h-[32px]">
-                <hr class="w-[30%] border-gray">
-                <p class="text-gray mb-1">or</p>
-                <hr class="w-[30%] border-gray">
-            </div>
-            
-            <form action="{{ route('register.process') }}" method="POST" class="flex flex-col gap-[16px] mb-[3rem]">
-                @csrf
-                <x-input-form type="text" name="name" icon="/asset/ic_user.svg" placeholder="Name"/>
-                <x-input-form type="email" name="email" icon="/asset/ic_email.svg" placeholder="Email"/>
-                <x-input-form type="password" name="password" icon="/asset/ic_password.svg" placeholder="Password"/>
-                <x-input-form type="password" name="password_confirmation" icon="/asset/ic_password.svg" placeholder="Confirm Password"/>
-                
-                <div class="flex gap-2 items-center">
-                    <input type="checkbox" name="remember_me" class="accent-gray checked:accent-primary h-[24px]">
-                    <p class="text-sm mb-0.5">I agree to
-                        <a href="" class="text-sm text-primary"> Terms & Conditions</a>
-                    </p>
+            {{-- form register ygy --}}
+            <div class="container bg-white px-[3rem] py-[1rem] w-3/4" style="transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;">
+                <div data-aos="fade-right" data-aos-delay="300" class="flex mt-5 mb-2 items-center justify-start">
+                    <img src="{{ asset('asset\logo.svg') }}" alt="" class="w-10">
+                    <p class="text-primary text-2xl transform -translate-x-1.25">cale<b>Up</b></p>
                 </div>
-                
-                <button class="rounded-[12px] border-1 bg-primary-900 p-3 w-full font-bold text-white text-sm cursor-pointer">
-                    Sign Up
-                </button>
 
-                <p class="text-sm">
-                    Already have an account? 
-                    <a href="{{ route('login') }}" class="text-sm text-primary">Sign in</a>
-                </p>
-            </form>
+                <div data-aos="fade-up" data-aos-delay="500">   
+                    <h2 class="text-dark text-2xl text-center font-bold py-[1rem] mb-2">Sign Up</h2>
+                    <button class="rounded-[12px] border-1 border-gray h-[44px] w-full flex flex-row justify-center items-center gap-1 cursor-pointer hover:border-primary hover:scale-105 transition">
+                        <img src="/asset/ic_google.svg" alt="" style="width: 12px;">
+                        <p class="text-dark text-xs font-bold mb-0.5">Sign up with Google</p>
+                    </button>
+
+                    <div class="flex items-center justify-center gap-x-[8px] h-[32px]">
+                        <hr class="w-[30%] border-gray">
+                        <p class="text-gray text-xs mb-1">or</p>
+                        <hr class="w-[30%] border-gray">
+                    </div>
+                    
+                    <form action="{{ route('register.process') }}" method="POST" class="flex flex-col gap-[16px] mb-[2rem]">
+                        @csrf
+                        <x-input-form autocomplete="off" type="text" name="name" icon="/asset/ic_user.svg" placeholder="Name"/>
+                        <x-input-form autocomplete="off" type="email" name="email" icon="/asset/ic_email.svg" placeholder="Email"/>
+                        <div class="flex gap-[0.7rem]">
+                            <x-input-form autocomplete="off" type="password" name="password" icon="/asset/ic_password.svg" placeholder="Password"/>
+                            <x-input-form autocomplete="off" type="password" name="password_confirmation" icon="/asset/ic_password.svg" placeholder="Confirm Password"/>
+                        </div>
+
+                        <div data-aos="zoom-in" data-aos-delay="800" class="w-full flex flex-col gap-[16px]">
+                            <div class="flex gap-2 items-center mt-4">
+                                <input type="checkbox" name="remember_me" class="accent-gray checked:accent-primary h-[24px]">
+                                <p class="text-xs mb-0.5">I agree to
+                                    <a href="" class="text-xs text-primary"> Terms & Conditions</a>
+                                </p>
+                            </div>
+                            
+                            <x-custom-button type="submit" size="md"><span class="text-sm font-bold">Sign Up</span></x-custom-button>
+
+                            <p class="text-xs">
+                                Already have an account? 
+                                <a href="{{ route('login') }}" class="text-xs text-primary">Sign in</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </body>
@@ -103,8 +121,11 @@
         transition: transform 0.3s ease-in-out, background-color 0.3s ease-in-out;
     }
     .circle.current{
-        transform: scale(1.3, 1.3);
-        background-color: white;
+        width: 18px;
+        height: 9px;
+        border-radius: 8px;
+        transition: all 0.3s ease-in-out;
+        background: white;
     }
 </style>
 <script>
